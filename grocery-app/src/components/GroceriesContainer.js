e9e155b64ea08e1821bccb82f85537bed7626576
Silvia Rocha <silviadprocha@gmail.com>
2020-05-02 23:07:27 +0900

    add update function

--- grocery-app/src/components/GroceriesContainer.js
+++ grocery-app/src/components/GroceriesContainer.js
@@ -45,6 +45,23 @@ class GroceriesContainer extends Component {
     this.setState({ inputValue: e.target.value });
   };
 
+  updateGrocery = (e, id) => {
+    axios
+      .put(`/api/v1/groceries/${id}`, { grocery: { done: e.target.checked } })
+      .then((response) => {
+        const groceryIndex = this.state.groceries.findIndex(
+          (x) => x.id === response.data.id
+        );
+        const groceries = update(this.state.groceries, {
+          [groceryIndex]: { $set: response.data },
+        });
+        this.setState({
+          groceries: groceries,
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+
   render() {
     return (
       <div>
@@ -64,7 +81,12 @@ class GroceriesContainer extends Component {
             {this.state.groceries.map((grocery) => {
               return (
                 <li className="grocery" grocery={grocery} key={grocery.id}>
-                  <input className="groceryCheckbox" type="checkbox" />
+                  <input
+                    className="groceryCheckbox"
+                    type="checkbox"
+                    checked={grocery.done}
+                    onChange={(e) => this.updateGrocery(e, grocery.id)}
+                  />
                   <label className="groceryLabel">{grocery.title}</label>
                   <span className="deleteGroceryBtn">x</span>
                 </li>
