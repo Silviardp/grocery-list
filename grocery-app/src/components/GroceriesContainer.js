c1bc8d5a8e90415b5d8e8a2e8294a1eefcc64dba
Silvia Rocha <silviadprocha@gmail.com>
2020-05-02 22:52:53 +0900

    add create groceries

--- grocery-app/src/components/GroceriesContainer.js
+++ grocery-app/src/components/GroceriesContainer.js
@@ -1,6 +1,50 @@
 import React, { Component } from "react";
+import axios from "axios";
+import update from "immutability-helper";
 
 class GroceriesContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groceries: [],
+      inputValue: "",
+    };
+  }
+
+  getGroceries() {
+    axios
+      .get("/api/v1/groceries")
+      .then((response) => {
+        this.setState({ groceries: response.data });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  componentDidMount() {
+    this.getGroceries();
+  }
+
+  createGrocery = (e) => {
+    if (e.key === "Enter") {
+      axios
+        .post("/api/v1/groceries", { grocery: { title: e.target.value } })
+        .then((response) => {
+          const groceries = update(this.state.groceries, {
+            $splice: [[0, 0, response.data]],
+          });
+          this.setState({
+            groceries: groceries,
+            inputValue: "",
+          });
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
   render() {
     return (
       <div>
@@ -10,10 +54,23 @@ class GroceriesContainer extends Component {
             type="text"
             placeholder="Add an item"
             maxLength="50"
+            onKeyPress={this.createGrocery}
+            value={this.state.inputValue}
+            onChange={this.handleChange}
           ></input>
         </div>
         <div className="ListWrapper">
-          <ul className="groceryList"></ul>
+          <ul className="groceryList">
+            {this.state.groceries.map((grocery) => {
+              return (
+                <li className="grocery" grocery={grocery} key={grocery.id}>
+                  <input className="groceryCheckbox" type="checkbox" />
+                  <label className="groceryLabel">{grocery.title}</label>
+                  <span className="deleteGroceryBtn">x</span>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     );
